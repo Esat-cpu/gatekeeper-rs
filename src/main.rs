f199051887db01424aecc9a87732792120ac7a71
Esat <saygina7@gmail.com>
2026-02-22 19:45:10 +0300

    feat: add sign up functionality
    Get username from user and validate it.
    Then get password, validate and hash it.
    Write both credentials to 'vault.csv' file as <username>::<hash>.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::io::{self, Write};
+use argon2::{Argon2, PasswordHasher};
+use argon2::password_hash::SaltString;
+use rand::thread_rng;
+use std::fs::OpenOptions;
+
 
 fn main() {
+    // User chooses to login or sign up
     let choice: String = loop {
         let mut choice = String::new();
 
@@ -21,12 +27,73 @@ fn main() {
     };
 
 
+
     if choice == "1" {
         println!("1");
     }
 
+
+
     else if choice == "2" {
-        println!("2");
+        // Get username from user
+        let username: String = loop {
+            let mut input = String::new();
+
+            print!("Enter your username: ");
+            io::stdout().flush().unwrap();
+
+            io::stdin()
+                .read_line(&mut input)
+                .unwrap();
+
+            let input = input.trim();
+
+
+            if input.len() < 2 || input.len() > 20 {
+                eprintln!("Username must be between 2 and 20 characters.");
+                continue;
+            }
+
+            if !input.chars().all(|c| c.is_alphanumeric() || c == '_') {
+                eprintln!("Username can only contain letters, numbers and '_'.");
+                continue;
+            }
+
+            break input.to_string();
+        };
+
+
+        // Get password from user
+        let password: String = loop {
+            let pass = rpassword::prompt_password("Enter your password: ").unwrap();
+
+            if pass.len() < 6 || pass.len() > 50 {
+                eprintln!("Password must be between 6 and 50 characters.");
+                continue;
+            }
+
+            break pass;
+        };
+
+
+        // Hashing the password
+        let salt = SaltString::generate(&mut thread_rng());
+
+        let hash = Argon2::default()
+            .hash_password(password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
+
+
+        // Write the username and the password hash to the file
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("vault.csv")
+            .unwrap();
+
+        writeln!(&mut file, "{}::{}", username, hash).unwrap();
+        println!("Sign up successful!");
     }
 }
 
